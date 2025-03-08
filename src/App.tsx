ef9d6e10fc1ba0c3118ae81a68fd07023e7945ad
geeky-auro <aurosaswat.apk@gmail.com>
2025-03-08 18:58:44 +0530

    Header and Footer Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <ListEmployeeComponent />
+      <Footer />
     </>
   );
 }
