d52b79e1177c1b8d1c2d353c1ed301d8c1a3413c
geeky-auro <aurosaswat.apk@gmail.com>
2025-03-08 21:56:21 +0530

    Adding Router in EMS

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
-      <ListEmployeeComponent />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />} />
+          <Route path="/employee" element={<ListEmployeeComponent />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <ListEmployeeComponent />
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
